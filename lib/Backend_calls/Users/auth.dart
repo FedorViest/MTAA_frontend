46ea2af790137e74df76938c76ba1e3a2eca5cd2
jakubsmorada <xsmorada@stuba.sk>
2022-04-13 16:18:25 +0200

    added backend calls for signing in users

---
+++ lib/Backend_calls/Users/auth.dart
@@ -0,0 +1,30 @@
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:jwt_decode/jwt_decode.dart';
+import 'package:provider/provider.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+class Auth with ChangeNotifier{
+  var dio = Dio();
+  login(String email, String password) async {
+    try{
+      dio.options.headers['content-Type'] = 'application/x-www-form-urlencoded';
+      FormData formData = FormData.fromMap(
+          {
+            'username': email,
+            'password': password
+          }
+      );
+      Response response = await dio.post('http://10.0.2.2:8000/users/login', data: formData);
+      print(response.data);
+      final prefs = await SharedPreferences.getInstance();
+      prefs.setString('access_token', response.data["access_token"]);
+      Map<String, dynamic> data = Jwt.parseJwt(response.data["access_token"]);
+
+      return response.data;
+    }
+    catch (e){
+      print(e);
+    }
+  }
+}
--- lib/Screens/login.dart
+++ lib/Screens/login.dart
@@ -1,8 +1,13 @@
+import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
+import 'package:frontend/Backend_calls/Users/login_calls.dart';
 import 'package:frontend/Screens/customer.dart';
 import 'package:frontend/Screens/employee.dart';
 import 'package:frontend/Screens/register.dart';
 import 'package:frontend/Screens/admin.dart';
+import 'package:provider/provider.dart';
+
+import '../Backend_calls/Users/auth.dart';
 
 enum Positions{
   admin,
@@ -22,23 +27,29 @@ class LoginScreen extends StatefulWidget{
 class _LoginScreenState extends State<LoginScreen> {
   final _formKey = GlobalKey<FormState>();
   String? _position;
-  
+  final emailController = TextEditingController();
+  final passwordController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
 
-    handleUser() async{
+    handleUser() async {
+      var dio = Dio();
       print(_position);
-      if (_position == positions[0]){
-        return Navigator.of(context).push(MaterialPageRoute(builder: (context) => AdminScreen()));
+      Provider.of<Auth>(context, listen: false).login(emailController.text, passwordController.text);
+
+      var response = await Users().getInfo();
+
+      print('->>>> ${response["position"]}');
+
+      if (response["position"] == "admin"){
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => AdminScreen()));
       }
-      else if(_position == positions[1]){
-        return Navigator.of(context).push(MaterialPageRoute(builder: (context) => EmployeeScreen()));
+      else if (response["position"] == "employee"){
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => EmployeeScreen()));
       }
-      else if(_position == positions[2]){
-        return Navigator.of(context).push(MaterialPageRoute(builder: (context) => CustomerScreen()));
-      }
-      else{
-        return null;
+      else if (response["position"] == "customer"){
+        Navigator.of(context).push(MaterialPageRoute(builder: (context) => CustomerScreen()));
       }
     }
 
@@ -46,115 +57,117 @@ class _LoginScreenState extends State<LoginScreen> {
 
     return Scaffold(
       body: SingleChildScrollView(
-      child: Container(
-        color: Color(0xFF133B5C),
-        height: size.height,
-        child: Form(
-          key: _formKey,
-          child: Column(
-            children: <Widget>[
-              Container(
-                height: size.height * 0.3,
-                color: Color(0xFF1E5F74)
-              ),
-              Container(
-                alignment: Alignment.topCenter,
-                margin: EdgeInsets.only(top: 20),
-              ),
-
-              Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(left: 20, right: 20, top: 30),
-                child: TextFormField(
-                  style: TextStyle(
-                    color: Colors.white,
-                  ),
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      filled: true,
-                      hintStyle: TextStyle(color: Colors.grey[800]),
-                      hintText: "Email",
-                      fillColor: Colors.white70),
-                      validator: (value){
-                        if(value == null || value.isEmpty){
-                          return "This field is required.";
-                        }else{
-                          _position = value;
-                        }
-                        },
+        child: Container(
+          color: Color(0xFF133B5C),
+          height: size.height,
+          child: Form(
+            key: _formKey,
+            child: Column(
+              children: <Widget>[
+                Container(
+                    height: size.height * 0.3,
+                    color: Color(0xFF1E5F74)
                 ),
-              ),
-              Container(
-                alignment: Alignment.center,
-                margin: EdgeInsets.only(left: 20, right: 20, top: 30),
-                child: TextFormField(
-                  decoration: InputDecoration(
-                      border: OutlineInputBorder(
-                        borderRadius: BorderRadius.circular(10.0),
-                      ),
-                      filled: true,
-                      hintStyle: TextStyle(color: Colors.grey[800]),
-                      hintText: "Password",
-                      fillColor: Colors.white70),
-                      obscureText: true,
-                      validator: (value){
-                        if(value == null || value.isEmpty){
-                          return "This field is required.";
-                        }
-                  },
-                ),
-              ),
-
-              SizedBox(height: size.height * 0.05),
-
-              Container(
-                alignment: Alignment.center,
-                child: ElevatedButton(
-                  onPressed: () {
-                    // Validate returns true if the form is valid, or false otherwise.
-                    if (_formKey.currentState!.validate()) {
-                      // If the form is valid, display a snackbar. In the real world,
-                      // you'd often call a server or save the information in a database.
-                      ScaffoldMessenger.of(context).showSnackBar(
-                        const SnackBar(content: Text('Processing Data')),
-                      );
-                      handleUser();
-                    }
-                  },
-                  child: const Text('SIGN IN'),
-                  style: ElevatedButton.styleFrom(
-                      fixedSize: const Size(200, 60), primary: Color(0xFF1E5F74)),
-                ),
+                Container(
+                  alignment: Alignment.topCenter,
+                  margin: EdgeInsets.only(top: 20),
                 ),
 
-              SizedBox(height: size.height * 0.01),
-
-              Container(
-                alignment: Alignment.center,
-                child: TextButton(
-                    onPressed: () {
-                      Navigator.of(context).push(MaterialPageRoute(builder: (context) => RegisterScreen()));
-                    },
-                    child: RichText(
-                      text: const TextSpan(
-                        style: TextStyle(
-                          fontSize: 14,
-                          color: Colors.grey,
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 30),
+                  child: TextFormField(
+                    controller: emailController,
+                    style: TextStyle(
+                      color: Colors.white,
+                    ),
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
                         ),
-                        children: <TextSpan>[
-                          TextSpan(text: 'Don\'t have an account? '),
-                          TextSpan(text: 'Register here!', style: TextStyle(fontWeight: FontWeight.bold)),
-                        ],
-                      ),
+                        filled: true,
+                        hintStyle: TextStyle(color: Colors.grey[800]),
+                        hintText: "Email",
+                        fillColor: Colors.white70),
+                    validator: (value){
+                      if(value == null || value.isEmpty){
+                        return "This field is required.";
+                      }else{
+                        _position = value;
+                      }
+                    },
+                  ),
+                ),
+                Container(
+                  alignment: Alignment.center,
+                  margin: EdgeInsets.only(left: 20, right: 20, top: 30),
+                  child: TextFormField(
+                    controller: passwordController,
+                    decoration: InputDecoration(
+                        border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(10.0),
+                        ),
+                        filled: true,
+                        hintStyle: TextStyle(color: Colors.grey[800]),
+                        hintText: "Password",
+                        fillColor: Colors.white70),
+                    obscureText: true,
+                    validator: (value){
+                      if(value == null || value.isEmpty){
+                        return "This field is required.";
+                      }
+                    },
+                  ),
+                ),
+
+                SizedBox(height: size.height * 0.05),
+
+                Container(
+                  alignment: Alignment.center,
+                  child: ElevatedButton(
+                    onPressed: () {
+                      // Validate returns true if the form is valid, or false otherwise.
+                      if (_formKey.currentState!.validate()) {
+                        // If the form is valid, display a snackbar. In the real world,
+                        // you'd often call a server or save the information in a database.
+                        ScaffoldMessenger.of(context).showSnackBar(
+                          const SnackBar(content: Text('Processing Data')),
+                        );
+                        handleUser();
+                      }
+                    },
+                    child: const Text('SIGN IN'),
+                    style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(200, 60), primary: Color(0xFF1E5F74)),
+                  ),
+                ),
+
+                SizedBox(height: size.height * 0.01),
+
+                Container(
+                    alignment: Alignment.center,
+                    child: TextButton(
+                        onPressed: () {
+                          Navigator.of(context).push(MaterialPageRoute(builder: (context) => RegisterScreen()));
+                        },
+                        child: RichText(
+                          text: const TextSpan(
+                            style: TextStyle(
+                              fontSize: 14,
+                              color: Colors.grey,
+                            ),
+                            children: <TextSpan>[
+                              TextSpan(text: 'Don\'t have an account? '),
+                              TextSpan(text: 'Register here!', style: TextStyle(fontWeight: FontWeight.bold)),
+                            ],
+                          ),
+                        )
                     )
-                )
-              ),
-            ],
+                ),
+              ],
+            ),
           ),
         ),
-      ),
       ),
     );
   }
