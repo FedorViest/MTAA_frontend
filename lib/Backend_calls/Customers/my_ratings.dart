6d083d1cf804f35a58b2ab138d5c513a6d77cdc8
jakubsmorada <xsmorada@stuba.sk>
2022-04-15 20:54:56 +0200

    finished my ratings screen for customer

---
+++ lib/Backend_calls/Customers/my_ratings.dart
@@ -0,0 +1,70 @@
+import 'dart:convert';
+
+import 'package:dio/dio.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../Utils/constants.dart';
+
+class Rating {
+  int id;
+  String employee_email;
+  double rating;
+  String comment;
+
+  Rating(this.id, this.employee_email, this.rating, this.comment);
+}
+
+class getRatings with ChangeNotifier {
+  var dio = Dio();
+
+  getInfo() async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final access_token = prefs.getString("access_token") ?? '';
+      dio.options.headers["authorization"] = "Bearer " + access_token;
+
+      Response response =
+      await dio.get(url + '/customer/getRatings');
+
+      print(response);
+
+      late List<Rating> ratings = [];
+
+      for (var item in response.data) {
+        var order = Rating(item["Ratings"]["id"], item["employee_email"],
+            item["Ratings"]["rating"], item["Ratings"]["comment"]);
+        ratings.add(order);
+        print(item["employee_email"]);
+      }
+      for (var item in ratings) {
+        print(item);
+      }
+
+      return ratings;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
+
+class deleteRating{
+  var dio = Dio();
+
+  delete_rating(int rating_id) async {
+    try {
+      final prefs = await SharedPreferences.getInstance();
+      final access_token = prefs.getString("access_token") ?? '';
+      dio.options.headers["authorization"] = "Bearer " + access_token;
+
+      Response response = await dio
+          .delete(url + '/customer/removeRating/$rating_id');
+
+      print(response);
+
+      return response;
+    } catch (e) {
+      print(e);
+    }
+  }
+}
---
+++ lib/Screens/Customer/My_ratings/my_ratings.dart
@@ -0,0 +1,141 @@
+import 'package:flutter/material.dart';
+import 'package:frontend/Backend_calls/Customers/my_ratings.dart';
+import 'package:frontend/Backend_calls/Employees/update_repair.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+import '../../Users/profile.dart';
+import '../../../Backend_calls/Employees/get_repairs.dart';
+
+class MyRatingsScreen extends StatefulWidget {
+  final List<Rating> ratings;
+
+  const MyRatingsScreen({Key? key, required this.ratings}) : super(key: key);
+
+  @override
+  _MyRatingsScreenState createState() => _MyRatingsScreenState();
+}
+
+class _MyRatingsScreenState extends State<MyRatingsScreen> {
+  int _selectedIndex = 0;
+  int _selectedId = 0;
+  late List<Rating> ratings = widget.ratings;
+
+  @override
+  Widget build(BuildContext context) {
+    //print("Date: ${response["Orders"]["date_created"]}, Status: ${response["Orders"]["status"]}");
+
+    Size size = MediaQuery
+        .of(context)
+        .size;
+
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Color(0xFF1E5F74),
+      ),
+      body: SingleChildScrollView(
+        child: Container(
+          height: size.height,
+          color: Color(0xFF133B5C),
+          child: Column(
+            children: [
+              Profile(),
+              Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  SizedBox(height: size.height * 0.1),
+                  Container(
+                    decoration: const BoxDecoration(
+                      color: Color(0xFF133B5C),
+                    ),
+                    child: SizedBox(
+                      height: size.height * 0.5,
+                      child: ListView.builder(
+                        padding: EdgeInsets.all(10),
+                        itemCount: ratings.length,
+                        scrollDirection: Axis.vertical,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Card(
+                            color: index % 2 == 0
+                                ? Color(0xFF1E5F74)
+                                : Color(0xFFFCDAB7),
+                            child: Padding(
+                              padding: const EdgeInsets.all(0),
+                              child: ListTile(
+                                selected: index == _selectedIndex,
+                                selectedTileColor: Color(0xffc8a2c8),
+                                selectedColor: Color(0xFFFCDAB7),
+                                onTap: () {
+                                  setState(() {
+                                    _selectedIndex = index;
+                                    _selectedId = ratings[_selectedIndex].id;
+                                    print(_selectedId);
+                                  });
+                                },
+                                onLongPress: () =>
+                                {
+                                  setState(() {
+                                    _selectedIndex = index;
+                                  }),
+                                  showDialog(
+                                    context: context,
+                                    builder: (context) =>
+                                        AlertDialog(
+                                          title: Center(
+                                              child: Text("Order information",
+                                                  style: const TextStyle(
+                                                      color: Colors.white))),
+                                          content: Text(
+                                              "",
+                                              style: const TextStyle(
+                                                  color: Colors.white,
+                                                  fontWeight: FontWeight.bold,
+                                                  fontSize: 15)),
+                                          backgroundColor: Color(0xFF133B5C),
+                                        ),
+                                  )
+                                },
+                                horizontalTitleGap: 30,
+                                leading: Text(
+                                  ratings[index].employee_email,
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                ),
+                                trailing: Text(
+                                  ratings[index].rating.toString(),
+                                  style: const TextStyle(
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ),
+                            ),
+                          );
+                        },
+                      ),
+                    ),
+                  ),
+                  SizedBox(height: size.height * 0.06),
+                  ElevatedButton(
+                    onPressed: () {
+                      deleteRating().delete_rating(_selectedId);
+                      setState(() {});
+                    },
+                    style: ElevatedButton.styleFrom(
+                        fixedSize: const Size(240, 70),
+                        primary: Color(0xFF1E5F74)),
+                    child: const Text(
+                      "Delete selected rating",
+                      style: TextStyle(
+                          fontSize: 20, fontWeight: FontWeight.bold),
+                    ),
+                  ),
+                ],
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
