b141a6ed90f9479a5597ce30221b044203893507
FedorViest <fedo.viest@gmail.com>
2022-04-15 22:19:50 +0200

    Added settings icon to profile widget

--- lib/Screens/Users/profile.dart
+++ lib/Screens/Users/profile.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/Utils/constants.dart';
 
 class Profile extends StatefulWidget {
   @override
@@ -14,11 +15,14 @@ class _ProfileState extends State<Profile>{
         alignment: Alignment.topCenter,
         color: Color(0xFF1D2D50),
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          SizedBox(height: size.height * 0.02),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.spaceAround,
-            crossAxisAlignment: CrossAxisAlignment.start,
+          Padding(
+          //SizedBox(height: size.height * 0.02),
+          padding: EdgeInsets.fromLTRB(15, 0, 0, 0),
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: <Widget>[
               CircleAvatar(
                 backgroundColor: Colors.blue,
@@ -32,16 +36,16 @@ class _ProfileState extends State<Profile>{
                     color: Colors.white
                 ),
               ),
-              Text(
-                "customer@gmail.com",
-                style: TextStyle(
-                    fontSize: size.width / 20,
-                    fontWeight: FontWeight.bold,
-                    color: Colors.white
-                ),
+              IconButton(
+                  highlightColor: COLOR_CREAM,
+                  iconSize: 50,
+                  onPressed: () {},
+                  icon: Icon(
+                      Icons.settings_applications),
               ),
             ],
           ),
+    ),
         ],
         ),
       );
