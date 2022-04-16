dd2bc31b717c9161e70ba225b4fc7a8580a6d5d8
FedorViest <fedo.viest@gmail.com>
2022-04-16 13:08:29 +0200

    Added splashscreen

---
+++ lib/splash.dart
@@ -0,0 +1,31 @@
+import 'dart:async';
+import 'package:flutter/material.dart';
+import 'package:frontend/Screens/Users/login.dart';
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(
+        Duration(seconds: 3),
+            () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => LoginScreen())));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF133B5C),
+      body: Container(
+        margin: EdgeInsets.only(top: 200),
+        alignment: Alignment.topCenter,
+        child: Image.asset('assets/splashscreen/logo.png'),
+      ),
+    );
+  }
+}
