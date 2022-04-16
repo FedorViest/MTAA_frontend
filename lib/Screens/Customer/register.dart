7af8de48c1f6a10f95c2b3c1746f83cd479d9d5f
FedorViest <fedo.viest@gmail.com>
2022-04-16 18:09:16 +0200

    Created popup window on successful registration

--- lib/Screens/Customer/register.dart
+++ lib/Screens/Customer/register.dart
@@ -16,6 +16,33 @@ class _RegisterScreenState extends State<RegisterScreen> {
   final passwordController = TextEditingController();
   final nameController = TextEditingController();
 
+  Future<bool> _showMyDialog() async {
+    return await showDialog(
+      context: context,
+      barrierDismissible: false, // user must tap button!
+      builder: (BuildContext context) {
+        return AlertDialog(
+          backgroundColor: Color(0xFFFCDAB7),
+          title: const Text('Successfully created account'),
+          alignment: Alignment.center,
+          actions: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.pop(context);
+                Navigator.of(context).push(
+                    MaterialPageRoute(builder: (context) => LoginScreen()));
+              },
+              child: Text(
+                "OK",
+                style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+              ),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
@@ -71,25 +98,24 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   alignment: Alignment.center,
                   margin: EdgeInsets.only(left: 20, right: 20, top: 30),
                   child: TextFormField(
-                    controller: emailController,
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
-                    decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(10.0),
-                        ),
-                        filled: true,
-                        hintStyle: TextStyle(color: Colors.grey[800]),
-                        hintText: "Email",
-                        fillColor: Colors.white70),
-                    validator: MultiValidator([
-                      EmailValidator(
-                        errorText: "Enter valid email (example@example.com).",
+                      controller: emailController,
+                      style: TextStyle(
+                        color: Colors.white,
                       ),
-                      RequiredValidator(errorText: "This field is required."),
-                    ])
-                  ),
+                      decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(10.0),
+                          ),
+                          filled: true,
+                          hintStyle: TextStyle(color: Colors.grey[800]),
+                          hintText: "Email",
+                          fillColor: Colors.white70),
+                      validator: MultiValidator([
+                        EmailValidator(
+                          errorText: "Enter valid email (example@example.com).",
+                        ),
+                        RequiredValidator(errorText: "This field is required."),
+                      ])),
                 ),
                 Container(
                   alignment: Alignment.center,
@@ -108,7 +134,7 @@ class _RegisterScreenState extends State<RegisterScreen> {
                         hintText: "Password",
                         fillColor: Colors.white70),
                     obscureText: true,
-                    validator:  (value) {
+                    validator: (value) {
                       if (value == null || value.isEmpty) {
                         return "This field is required.";
                       }
@@ -120,45 +146,48 @@ class _RegisterScreenState extends State<RegisterScreen> {
                   alignment: Alignment.center,
                   child: ElevatedButton(
                     onPressed: () async {
-
                       if (_formKey.currentState!.validate()) {
-                      var response = await Register().register(nameController.text, emailController.text, passwordController.text);
-                      if (response.runtimeType == DioError) {
-                        String errorMessage = Map<String, dynamic>.from(
-                            response.response.data)["detail"].toString();
-                        if (errorMessage == "User with selected email already exists") {
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text(
-                                'Email already in use',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold
-                                ),),
-                              backgroundColor: Colors.red,
-                            ),
+                        var response = await Register().register(
+                            nameController.text,
+                            emailController.text,
+                            passwordController.text);
+                        if (response.runtimeType == DioError) {
+                          String errorMessage = Map<String, dynamic>.from(
+                                  response.response.data)["detail"]
+                              .toString();
+                          if (errorMessage ==
+                              "User with selected email already exists") {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text(
+                                  'Email already in use',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                backgroundColor: Colors.red,
+                              ),
+                            );
+                          } else {
+                            ScaffoldMessenger.of(context).showSnackBar(
+                              const SnackBar(
+                                content: Text(
+                                  'Invalid data',
+                                  style: TextStyle(
+                                      fontSize: 20,
+                                      fontWeight: FontWeight.bold),
+                                ),
+                                backgroundColor: Colors.red,
+                              ),
+                            );
+                          }
+                        } else {
+                          _showMyDialog();
+                          /*ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('Processing Data')),
                           );
-                        }
-                        else{
-                          ScaffoldMessenger.of(context).showSnackBar(
-                            const SnackBar(
-                              content: Text(
-                                'Invalid data',
-                                style: TextStyle(
-                                    fontSize: 20,
-                                    fontWeight: FontWeight.bold
-                                ),),
-                              backgroundColor: Colors.red,
-                            ),
-                          );
-                        }
-                      }
-                      else {
-                        ScaffoldMessenger.of(context).showSnackBar(
-                          const SnackBar(content: Text('Processing Data')),
-                        );
                           Navigator.of(context).push(MaterialPageRoute(
-                              builder: (context) => LoginScreen()));
+                              builder: (context) => LoginScreen()));*/
                         }
                       }
                     },
